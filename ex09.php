a1d5f4f4bf3063a41bd77a753e410b21bf27aa74
hamza <traorehamza407@gmail.com>
2025-04-03 13:53:54 +0200

    Vérifiecation si un mot est un palindrome

--- ex09.php
+++ ex09.php
@@ -8,3 +8,13 @@
 // sinon
 //   afficher 'non'
 
+
+$mot = "MONKEY.D.LUFFY";
+
+$motInverse = strrev($mot);
+if ($mot == $motInverse){
+    echo $mot . " est un palindrome.";
+} else {
+    echo $mot . " n'est pas un palindrome.";
+}
+
